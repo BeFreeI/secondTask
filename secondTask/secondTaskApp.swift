ecd7c3e369285d94727c9a5707afef7145b3c6fa
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-03 16:00:51 +0300

    Initial Commit

---
+++ secondTask/secondTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondTaskApp.swift
+//  secondTask
+//
+//  Created by  Pavel Nepogodin on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct secondTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
